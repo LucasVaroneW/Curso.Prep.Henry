f27d0c9b6f4ce2c8d8f74f9c009d150c9f916662
LucasVaroneW <lucasvaronebio@gmail.com>
2021-05-17 15:08:05 -0300

    Ultimastareas

--- 10-JS-Extra-Homeworks/homework/homework.js
+++ 10-JS-Extra-Homeworks/homework/homework.js
@@ -10,6 +10,12 @@ function deObjetoAmatriz(objeto){
       C: 3
     }) ➞ [["D", 1], ["B", 2], ["C", 3]]*/
   //Escribe tu código aquí
+
+arreglo=[]
+for ( i in objeto){
+      arreglo.push([i, objeto[i]]);
+    }
+return arreglo;
 }
 
 
@@ -18,14 +24,47 @@ function numberOfCharacters(string) {
   //en formato par clave-valor.
   //Ej: Recibe ---> "adsjfdsfsfjsdjfhacabcsbajda" || Devuelve ---> { a: 5, b: 2, c: 2, d: 4, f: 4, h:1, j: 4, s: 5 } 
   //Escribe tu código aquí
-}
 
+// var arreglo=[]
+
+// for (let i = 0; i < string.length; i++) {
+//   if (Object.keys(arreglo).includes(string[i])) {
+//         arreglo [string[i]] = [arreglo[i]] + 1
+//           continue;
+//   }
+//   arreglo[string[i]] = 1
+// }
+// return arreglo;
+// }
+
+
+  var obj = {}
+
+  for (let i = 0; i < string.length; i++) {  
+    if (Object.keys(obj).includes(string[i])) {
+      obj[string[i]] = obj[string[i]] + 1
+      continue;
+    }
+    obj[string[i]] = 1
+  }
+  return obj;
+}
 
 function capToFront(s) {
   //Realiza una función que reciba como parámetro un string y mueva todas las letras mayúsculas
   //al principio de la palabra.
   //Ejemplo: soyHENRY -> HENRYsoy
   //Escribe tu código aquí
+
+let cadena=''
+let cadena2=''
+for ( let i=0 ; i < s.length ; i++)
+      if (s[i] === s[i].toUpperCase()){
+        cadena += s[i];
+      }
+      else {cadena2+= s[i];
+    }
+return cadena.concat ('',cadena2);
 }
 
 
@@ -35,21 +74,47 @@ function asAmirror(str) {
   //pero con cada una de sus palabras invertidas, como si fuera un espejo.
   //Ej: Recibe ---> "The Henry Challenge is close!" || Devuelve ---> "ehT yrneH egnellahC si !esolc"
   //Escribe tu código aquí
-} 
 
+  var frase = str.split(' ').map(function (item) {
+    return item.split('').reverse().join('');
+  }).join(' ');
+  return frase;
+} 
 
 function capicua(numero){
   //Escribe una función, la cual recibe un número y determina si es o no capicúa.
   //La misma debe retornar: "Es capicua" si el número se número que se lee igual de 
   //izquierda a derecha que de derecha a izquierda. Caso contrario retorna "No es capicua"
   //Escribe tu código aquí
+
+if(numero.toString().split('').reverse().join('')==numero.toString()){
+    return "Es capicua";
+    }
+return "No es capicua";
 }
 
+/*  let cadena= numero.toString();
+  let cadenaInvertida = "";
+  
+  cadenaInvertida = cadena.split("").reverse().join("");
+  if(cadena == cadenaInvertida){
+        return "Es capicua"
+  }
+  return "No es capicua"
 
+}
+*/
 function deleteAbc(cadena){
   //Define una función que elimine las letras "a", "b" y "c" de la cadena dada 
   //y devuelva la versión modificada o la misma cadena, en caso de contener dichas letras.
   //Escribe tu código aquí
+let nuevacadena= '';
+  for (var i = 0; i < cadena.length; i++) {
+    if (cadena[i] !== 'a' && cadena[i] !== 'b'&& cadena[i] !== 'c'){
+    nuevacadena+=cadena[i];
+    }
+  }
+return nuevacadena;
 }
 
 
@@ -57,7 +122,18 @@ function sortArray(arr) {
   //La función recibe una matriz de strings. Ordena la matriz en orden creciente de longitudes de cadena
   //Ej: Recibe ---> ["You", "are", "beautiful", "looking"] || Devuelve ---> [“You", "are", "looking", "beautiful"]
   //Escribe tu código aquí
+  for (let i = 0; i < arr.length; i ++ ){
+    var temp = arr[i]
+    var j = i - 1
+    while (j >= 0 && temp.length < arr[j].length) {
+        arr[j+1] = arr[j]
+        j--
+    }
+    arr[j+1] = temp
 }
+  return arr;
+}
+
 
 
 function buscoInterseccion(arreglo1, arreglo2){
@@ -66,8 +142,16 @@ function buscoInterseccion(arreglo1, arreglo2){
   //Si no tienen elementos en común, retornar un arreglo vacío.
   //Aclaración: los arreglos no necesariamente tienen la misma longitud
   //Escribe tu código aquí  
+let arreglo=[]
+  for (let i = 0; i < arreglo1.length; i++) {
+    for (let j = 0; j < arreglo2.length; j++) {
+      if (arreglo1[i] === arreglo2[j]) {
+          arreglo.push(arreglo1[i])
+          }
+    }
+  }
+return arreglo;
 }
-
 
 
 // No modificar nada debajo de esta línea
